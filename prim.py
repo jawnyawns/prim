1b83715769767cd319d36811b8992c15c4dc7977
John Chin-Jew <johnchinjew@gmail.com>
2024-12-09 15:39:08 -0800

    use enums and refactor main

--- prim.py
+++ prim.py
@@ -1,34 +1,32 @@
 import sys
 
-SYMBOL_CHARS = "abcdefghijklmnopqrstuvwxyz_"
-NUMBER_CHARS = "0123456789"
-L_PAREN_CHAR = "("
-R_PAREN_CHAR = ")"
-SPACE_CHARS = " \n"
+from enum import Enum
+from typing import (
+    TextIO,
+)
 
-TOKEN_L_PAREN = "L_PAREN"
-TOKEN_R_PAREN = "R_PAREN"
-TOKEN_SYMBOL = "SYMBOL"
-TOKEN_NUMBER = "NUMBER"
+# TODO: Add debug level logging
+# TODO: Add type hints
+# TODO: Refactor tokenization
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python prim.py <SOURCE_FILE_PATH>")
-    else:
-        source_file_path = sys.argv[1]
+### CONSTANTS ###
 
-        with open(source_file_path, "r", encoding="utf-8") as source_file:
-            source_code = source_file.read()
-            source_tokens = tokenize(source_code)
+class CharSet(Enum):
+    SYMBOL = set("abcdefghijklmnopqrstuvwxyz_")
+    NUMBER = set("0123456789")
+    LPAREN = set("(")
+    RPAREN = set(")")
+    SPACE = set(" \n\t")
 
-            print(f"Tokenization success: {source_tokens}")
-
-            source_ast = parse(source_tokens)
-            evaluate(source_ast)
+class Token(Enum):
+    LPAREN = "LPAREN"
+    RPAREN = "RPAREN"
+    SYMBOL = "SYMBOL"
+    NUMBER = "NUMBER"
 
 ### TOKENIZATION ###
 
-def tokenize(source_code):
+def tokenize(source_code: str):
     tokens = []
     i = 0
     while i < len(source_code):
@@ -38,7 +36,7 @@ def tokenize(source_code):
             if token:
                 tokens.append(token)
         else:
-            raise Exception(f"Tokenization error: Invalid character {source_code[i]} at position {i}")
+            raise Exception(f"Tokenization error: Last valid token was {tokens[-1]} at position {i}")
     return tokens
 
 def next_token(i, source_code):
@@ -57,37 +55,37 @@ def next_token(i, source_code):
 
 def tokenize_space(i, source_code):
     char = source_code[i]
-    if char in SPACE_CHARS:
+    if char in CharSet.SPACE.value:
         return i + 1, None
     else:
         return i, None
 
 def tokenize_symbol(i, source_code):
-    if source_code[i] in SYMBOL_CHARS:
+    if source_code[i] in CharSet.SYMBOL.value:
         symbol = ""
-        while i < len(source_code) and source_code[i] in SYMBOL_CHARS:
+        while i < len(source_code) and source_code[i] in CharSet.SYMBOL.value:
             symbol += source_code[i]
             i += 1
-        return i, (TOKEN_SYMBOL, symbol)
+        return i, (Token.SYMBOL, symbol)
     else:
         return i, None
 
 def tokenize_number(i, source_code):
-    if source_code[i] in NUMBER_CHARS:
+    if source_code[i] in CharSet.NUMBER.value:
         number = ""
-        while i < len(source_code) and source_code[i] in NUMBER_CHARS:
+        while i < len(source_code) and source_code[i] in CharSet.NUMBER.value:
             number += source_code[i]
             i += 1
-        return i, (TOKEN_NUMBER, int(number))
+        return i, (Token.NUMBER, int(number))
     else:
         return i, None
 
 def tokenize_paren(i, source_code):
     char = source_code[i]
-    if char == L_PAREN_CHAR:
-        return i + 1, (TOKEN_L_PAREN, char)
-    elif char == R_PAREN_CHAR:
-        return i + 1, (TOKEN_R_PAREN, char)
+    if char in CharSet.LPAREN.value:
+        return i + 1, (Token.LPAREN, char)
+    elif char in CharSet.RPAREN.value:
+        return i + 1, (Token.RPAREN, char)
     else:
         return i, None
 
@@ -100,6 +98,28 @@ def parse(tokens):
 
 def evaluate(ast):
     print("Evaluating AST is not yet implemented!")
+
+### MAIN ###
+
+def main():
+    source_file_path = get_source_file_path(sys.argv)
+    with open(source_file_path, "r", encoding="utf-8") as source_file:
+        execute_source_file(source_file)
+
+def get_source_file_path(argv: list[str]):
+    if len(argv) == 2:
+        return argv[1]
+    else:
+        print("Usage: python prim.py <FILE_PATH>")
+
+def execute_source_file(source_file: TextIO):
+    source_code = source_file.read()
+    source_tokens = tokenize(source_code)
+
+    print(f"Debug: Tokenization success: {source_tokens}")
+
+    source_ast = parse(source_tokens)
+    evaluate(source_ast)
 
 if __name__ == "__main__":
     main()
